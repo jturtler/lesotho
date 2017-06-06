7daeff418564fb122c2e41ee0b8227212cc4a58f
chauthutran <cthutran@gmail.com>
2017-06-06 12:32:16 +0700

    Verion Uodate

--- WebContent/js/utils/lesotho_commons.js
+++ WebContent/js/utils/lesotho_commons.js
@@ -1,8 +1,8 @@
 
 function Commons() {}
 
-Commons.VERSION = "v0.22";
-Commons.VERSION_DATE = "Jun 05 2017";
+Commons.VERSION = "v0.23";
+Commons.VERSION_DATE = "Jun 06 2017";
 
 Commons.checkSession = function( returnFunc )
 {		
