9e18cd85f9f815aff3f98ae5540fe900ebb6ac08
chauthutran <cthutran@gmail.com>
2017-08-01 15:21:21 +0700

    Version changed

--- WebContent/js/utils/lesotho_commons.js
+++ WebContent/js/utils/lesotho_commons.js
@@ -14,6 +14,9 @@ Commons.monthYearFormat = "MMM YYYY";
 Commons.APPPAGE_COORDINATOR = "coordinator";
 Commons.APPPAGE_COUNSELLOR = "counsellor";
 
+Commons.sessionTimeOut = 60 * 60 * 1000;; // Get from [web.xml] configuration file
+Commons.intervalCheckSession = 5000; // 5 miliseconds;
+
 Commons.checkSession = function( returnFunc )
 {		
 	$.ajax( {
@@ -22,8 +25,14 @@ Commons.checkSession = function( returnFunc )
 		,dataType: "json"
         ,contentType: "application/json;charset=utf-8"
 		,success: function( response ) 
-		{					
-			returnFunc( !Commons.checkForSessionExpired( response ) );
+		{	
+			var valid = Commons.checkForSessionExpired( response );
+			if( valid )
+			{
+				Commons.sessionTimeOut = eval( response.sessionTimeOut ) * 60 * 1000; // convert minutes to miliseconds
+			}
+			
+			returnFunc( !valid );
 		}
 		,error: function(response)
 		{
@@ -37,4 +46,19 @@ Commons.checkForSessionExpired = function( response )
 {
 	return ( response && response.msg && response.msg == 'session_expired' );
 };
-	
+
+// -----------------------------------------------------------------------------
+// Check session timeout
+//------------------------------------------------------------------------------
+
+Commons.checkSessionTimeOut = function( returnFunc )
+{
+	Commons.sessionTimeOut = Commons.sessionTimeOut - Commons.intervalCheckSession;
+	var expired = ( Commons.sessionTimeOut <=0 );
+	returnFunc( expired );
+};
+
+Commons.ping = function( returnFunc )
+{
+	returnFunc( navigator.onLine );
+}; 
